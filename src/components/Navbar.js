42740b0f783089a8ea0490dc26f100395d732dbe
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-12-21 16:09:55 +0100

    Menu v 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,122 +2,182 @@ import { Mail, Notifications, Pets } from "@mui/icons-material";
 import {
   AppBar,
   Toolbar,
-  Typography,
   styled,
-  Badge,
-  Avatar,
-  Menu,
-  MenuItem,
-  InputBase,
   Button,
-  Container,
   useTheme,
+  Menu,
+  IconButton,
+  Typography,
+  MenuItem,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
 import { ButtonGr } from "./ui/ButtonGr";
 const pages = ["Home", "Relations investisseurs", "Contact"];
 
-const DeskStyle = styled(Box)(({theme})=>({
-  display:"flex",
+const DeskStyle = styled(Box)(({ theme }) => ({
+  display: "flex",
   justifyContent: "space-between",
-  [theme.breakpoints.down("lg")] :{
-    display:"none"
-}
+  [theme.breakpoints.down("lg")]: {
+    display: "none",
+  },
 }));
-const MobileStyle = styled(Box)(({theme})=>({
-  display:"none",
+const MobileStyle = styled(Box)(({ theme }) => ({
+  display: "none",
   justifyContent: "space-between",
-  [theme.breakpoints.down("lg")] :{
-    display:"flex"
-}
+  [theme.breakpoints.down("lg")]: {
+    display: "flex",
+  },
 }));
 
 export const Navbar = () => {
-  const [open, setOpen] = useState(false);
+  const [openNavMenu, setOpenNavMenu] = useState(false);
   const theme = useTheme();
   return (
     <AppBar
       position="static"
-      sx={{ backgroundColor: "white", boxShadow: "none",  }}
+      sx={{ backgroundColor: "white", boxShadow: "none" }}
     >
-      {/* <Container maxWidth="xl" > */}
-        <Toolbar
-          sx={{
-            height: "100px",
+      <Toolbar
+        sx={{
+          height: "100px",
+          display: "flex",
+          justifyContent: "space-between",
+          [theme.breakpoints.down("lg")]: {
+            height: "50px",
+            minHeight: "40px",
+            padding: "8px",
+          },
+        }}
+      >
+        <Box
+          component="img"
+          src="Grodesto H.png"
+          style={{
+            width: 159,
+            height: 37,
             display: "flex",
-            justifyContent: "space-between",
-            [theme.breakpoints.down("lg")] :{
-                  height:"50px",
-                  minHeight:"40px",
-                  padding:"8px",
-              },
-
           }}
-        >
+          alt="GroDesto LOGO"
+        />
+        <DeskStyle>
           <Box
-            component="img"
-            src="Grodesto H.png"
-            style={{
-              width: 159,
-              height: 37,
-              display:"flex",
-            }}
-            alt="GroDesto LOGO"
-          />
-          <DeskStyle>
-            <Box
-              sx={{
-                flexGrow: 1,
-             
-                marginRight: "79px",
-              }}
-            >
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  // onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 2,
-                    fontSize: 16,
-                    fontFamily: "Poppins",
-                    color: "#707070",
-                  }}
-                >
-                  {page}
-                </Button>
-              ))}
-            </Box>
-            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-              
-              <ButtonGr
-                title="Partenariat"
+            sx={{
+              flexGrow: 1,
 
-                variant="outlined"
-                style={{
-                  background: theme.palette.BlueGradient.main,
-                  background: theme.palette.BlueGradient.mainGradient,
-                  width: 164,
-                  height: 56,
-                  marginRight: 2,
+              marginRight: "79px",
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                // onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  fontSize: 16,
+                  fontFamily: "Poppins",
+                  color: "#707070",
                 }}
-               
-              ></ButtonGr>
-              <ButtonGr
-                title="Commencer à vendre"
-                variant="outlined"
-                style={{
-                  background: theme.palette.YellowGradient.main,
-                  background: theme.palette.YellowGradient.mainGradient,
-                  width: 260,
-                  height: 56,
-               
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <ButtonGr
+              title="Partenariat"
+              variant="outlined"
+              style={{
+                background: theme.palette.BlueGradient.main,
+                background: theme.palette.BlueGradient.mainGradient,
+                width: 164,
+                height: 56,
+                marginRight: 2,
+              }}
+            ></ButtonGr>
+            <ButtonGr
+              title="Commencer à vendre"
+              variant="outlined"
+              style={{
+                background: theme.palette.YellowGradient.main,
+                background: theme.palette.YellowGradient.mainGradient,
+                width: 260,
+                height: 56,
+              }}
+            ></ButtonGr>
+          </Box>
+        </DeskStyle>
+        <MobileStyle>
+          <IconButton
+            onClick={(e) => {
+              setOpenNavMenu(true);
+            }}
+          >
+            <MenuIcon sx={{ color: theme.palette.BlueGroDesto.main }} />
+          </IconButton>
+          <Menu
+            open={openNavMenu}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            onClose={(e) => setOpenNavMenu(false)}
+            
+            
+          >
+            <MenuItem sx={{display:"flex" , flexDirection:"column",}}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                // onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  fontSize: 10,
+                  fontFamily: "Poppins",
+                  color: "#707070",
                 }}
-               
-              ></ButtonGr>
-            </Box>
-            </DeskStyle>
-        </Toolbar>
+              >
+                {page}
+              </Button>
+            ))}
+            </MenuItem>
+            
+            
+              <MenuItem >
+               <ButtonGr
+              title="Partenariat"
+              variant="outlined"
+              style={{
+                background: theme.palette.BlueGradient.main,
+                background: theme.palette.BlueGradient.mainGradient,
+                width: 82,
+                height: 28,
+                fontSize: 5,
+                
+              }}
+            ></ButtonGr> 
+              </MenuItem>
+              <MenuItem>
+
+            <ButtonGr
+              title="Commencer à vendre"
+              variant="outlined"
+              style={{
+                background: theme.palette.YellowGradient.main,
+                background: theme.palette.YellowGradient.mainGradient,
+                width: 130,
+                height: 28,
+                fontSize: 5,
+                
+              }}
+            ></ButtonGr> 
+              </MenuItem>
+            
+            
+          </Menu>
+        </MobileStyle>
+      </Toolbar>
     </AppBar>
 
     // <div>
